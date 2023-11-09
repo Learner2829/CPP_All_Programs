073b7612b228a3d23196ff9168577f5f8bda5bd8
ASHISH G PATNI <136146658+Learner2829@users.noreply.github.com>
2023-11-09 19:59:16 +0530

    Update As-1_pr_8.cpp
    11/9/23

--- As-1_pr_8.cpp
+++ As-1_pr_8.cpp
@@ -8,6 +8,7 @@ using namespace std;
 //class which contains details of
 //subordinates of a manager. 
 //The employee object describing manager is to be passed as a parameter.
+class All_employee;
 class Employee{
 		int id;
 		string name;
@@ -33,34 +34,46 @@ class Employee{
 			cout<<"Manager : "<<manager<<endl;
 			
 		}
-		friend void pass(Employee e1[3]){
-			
-			for(int i=0;i<=3;i++)
-			{
-				e1[i].set_data();
-			}
-			for(int i=0;i<=3;i++)
-			{
-				e1[i].get_data();
-			}
-			
-			
-		}
+		friend All_employee getsubordinates(Employee e1,All_employee s1);
+	
 	};
 	
 		
 	
 	
 	class All_employee{
-		
+		string subordinates;
+		string manager;
+		public:
+		set(string input_manager,string input_subordinates){
+			subordinates=input_manager;
+			manager=input_manager;
+		}
+		get()
+		{
+			cout<<"Subordinates:"<<subordinates<<endl;
+			cout<<"Manager:"<<manager<<endl;
+			cout<<"----------------------------------"<<endl;
+			}	
 	};
+	 All_employee getsubordinates(Employee e1,All_employee s1)
+	{
+			string temp_manager;
+			string temp_subordinates;
+			temp_manager=e1.manager;
+			temp_subordinates=e1.subordinates;
+			
+			s1.set(temp_manager,temp_subordinates);
+			return s1;
+	}
 
 main()
 {
-	Employee e1;
+	Employee l1;
+	All_employee z1;
 	
-	e1.set_data();
-	e1.get_data();
-	
+	l1.set_data();
+	z1=getsubordinates(l1,z1);
+	z1.get();
 	
 }
